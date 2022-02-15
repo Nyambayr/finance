be0078b30d3093deecf22634f62c8d88260b6fbd
Nyambayr <nymkanyambayar99@gmail.com>
2022-02-15 19:19:20 +0900

    Expense,Income function setup

--- app.js
+++ app.js
@@ -1,18 +1,71 @@
 //displaing controll
-var uiController = (function () {})();
+var uiController = (function () {
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    addBtn: ".add__btn",
+  };
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+    gedDOMstrings: function () {
+      return DOMstrings;
+    },
+  };
+})();
 
 //finace controll
-var financeController = (function () {})();
+var financeController = (function () {
+  var Income = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var Expense = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+  var data = {
+    allItems: {
+      inc: [],
+      exp: [],
+    },
+    totals: {
+      inc: 0,
+      exp: 0,
+    },
+  };
+})();
 
 //linking controll
 var appController = (function (uiController, fnCotroller) {
-  var ctrlAddItem = function () {};
-  document.querySelector(".add__btn").addEventListener("click", function () {
-    ctrlAddItem();
-  });
-  document.addEventListener("keypress", function (event) {
-    if (event.keyCode === 13 || event.which === 13) {
+  var ctrlAddItem = function () {
+    console.log(uiController.getInput());
+  };
+  var setupEventListeners = function () {
+    var DOM = uiController.gedDOMstrings();
+    document.querySelector(DOM.addBtn).addEventListener("click", function () {
       ctrlAddItem();
-    }
-  });
+    });
+    document.addEventListener("keypress", function (event) {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+  return {
+    init: function () {
+      console.log("Application started...");
+      setupEventListeners();
+    },
+  };
 })(uiController, financeController);
+appController.init();
